78ce429694f473795a1efff4059f89785b21ace0
leandro-pessoa <119120060+leandro-pessoa@users.noreply.github.com>
2024-04-09 14:45:15 -0300

    remoção da rota about

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import DefaultPage from './pages/DefaultPage'
 import Home from './pages/Home'
 import Products from './pages/Products'
-import About from './pages/About'
 
 const AppRoutes = () => {
     return (
@@ -14,7 +13,6 @@ const AppRoutes = () => {
                 <Route path='/' element={ <DefaultPage /> }>
                     <Route index element={ <Home /> }/>
                     <Route path='produtos' element={ <Products /> }/>
-                    <Route path='sobre' element={ <About /> }/>
                 </Route>
             </Routes>
         </BrowserRouter>
